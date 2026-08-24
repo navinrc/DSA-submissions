a6ce3fd76adbb331c236dc9bba32364ab7ee84a9
Navin R C <40256832+navinrc@users.noreply.github.com>
2026-08-24 10:51:23 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # two pointers - O(1) space and O(n) time [in place]
+        l = 0
+        r = len(s) - 1
+        s = s.lower()
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l] != s[r]:
+                return False
+            if l >=r:
+                return True
+            l += 1
+            r -= 1
+        return True
+            

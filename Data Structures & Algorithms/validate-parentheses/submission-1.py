b9fe0103d87bf8ff6ee71bb9c576caea5c8f5fff
Navin R C <40256832+navinrc@users.noreply.github.com>
2026-09-03 13:01:44 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        close_to_open = {")": "(", "}": "{", "]": "["}
+        for c in s:
+            if c in close_to_open: # checks if it is a closing paranthesis
+                if stk and stk[-1] == close_to_open[c]: # check if valid pairs
+                    stk.pop()
+                else:
+                    return False # not matching brackets
+            else: # open paranthesis can be added directly as per - (((( or [[[
+                stk.append(c)
+
+        return len(stk) == 0

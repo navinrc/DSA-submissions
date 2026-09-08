ca004becbcb867963d998dea4a26c8f16dec0e3d
Navin R C <40256832+navinrc@users.noreply.github.com>
2026-09-08 14:23:09 +0530

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        start = 0
+        max_fruits = 0
+        state = {}
+
+        for end in range(len(fruits)):
+            state[fruits[end]] = state.get(fruits[end],0) + 1
+
+            while len(state) > 2: # shrink until valid
+                state[fruits[start]] -= 1
+                if state[fruits[start]] == 0:
+                    del state[fruits[start]]
+                start += 1
+            
+            max_fruits = max(max_fruits, end - start + 1)
+        return max_fruits
